3e94dc4dc56b3f7990530f73d7e15972c1544fa4
AsbjornPettersen <asbgpe@gmail.com>
2017-05-23 12:37:35 +0200

    useing new functions

--- mchf-eclipse/unittests/cattest/cattest.cpp
+++ mchf-eclipse/unittests/cattest/cattest.cpp
@@ -13,7 +13,6 @@
 #define USE_FULL_ASSERT
 
 
-
 uint32_t __builtin_bswap32(uint32_t val) // need to convert big / little endian!!
 {
   return val; // tbd
@@ -31,17 +30,66 @@ int __QADD(int i, int b) { return 0; }
 int __QSUB(int i, int b) { return 0; }
 
 #if 1
-static uint32_t total_transmitt_Len = 0;
+class tr_data
+{
+public:
+  std::vector<uint8_t> tab; 
+  void dump() const
+  {
+    unsigned int i = 0;
+    for (auto it = tab.cbegin(); it != tab.cend(); ++it) 
+      {       
+	uint8_t c = *it;
+	printf ("0x%02x,",c);
+	i++;
+      }
+    printf ("\n");
+  };
+  void add(uint8_t* Buf,uint32_t Len) 
+  {
+    for (uint32_t i=0; i < Len; i++)
+      {
+	tab.push_back(*Buf);
+	Buf++;
+      }
+  };
+};
+
+tr_data transdata; 
  
 uint8_t CDC_Transmit_FS(uint8_t* Buf,uint32_t Len) 
 {
-  total_transmitt_Len += Len;
-  printf ("Transmitt %d : %d\n",Len,total_transmitt_Len);
-
+  transdata.add(Buf, Len);
+  //  printf ("Transmitt %d\n",Len);
   return 0; 
 }
 
 #include "../../drivers/cat/cat_driver.c"
+
+struct TransceiverState volatile ts;
+
+#if 0
+#include "../../drivers/ui/radio_management.c"
+#else
+bool RadioManagement_FmDevIs5khz()
+{
+    return (ts.flags2 & FLAGS2_FM_MODE_DEVIATION_5KHZ) != 0;
+}
+
+void RadioManagement_FmDevSet5khz(bool is5khz)
+{
+    if (is5khz)
+    {
+        ts.flags2 |= FLAGS2_FM_MODE_DEVIATION_5KHZ;
+    }
+    else
+    {
+        ts.flags2 &= ~FLAGS2_FM_MODE_DEVIATION_5KHZ;
+    }
+}
+#endif
+
+
 
 void __cdecl UiDriver_ToggleVfoAB(void) {}
 void __cdecl UiDriver_SetSplitMode(bool) {  }
@@ -55,290 +103,12 @@ USBD_HandleTypeDef hUsbDeviceFS;
 
 __IO CdcVcp_CtrlLines_t  cdcvcp_ctrllines;
 
-struct TransceiverState volatile ts;
 #endif
 
 bool okt()
 {
   return true;
 }
-
-TEST(SerialepromTest, addr) 
-{ 
-#if 0
-  //   EXPECT_EQ (true, okt ());
-  uint8_t devaddr;
-  uint32_t Addr = 0x10000; /* example */
-  uint8_t Mem_Type = 17;
-  
-  SerialEEPROM_24Cxx_AdjustAddrs(Mem_Type,&devaddr,&Addr);
-  EXPECT_EQ (MEM_DEVICE_WRITE_ADDR + 8, devaddr); 
-  
-  /* NB! Addr have been changed ! */
-  SerialEEPROM_24Cxx_AdjustAddrs(Mem_Type,&devaddr,&Addr);	
-  EXPECT_EQ (MEM_DEVICE_WRITE_ADDR + 8, devaddr); 
-  /*---------------------------------------------------*/
-  /* devaddr will be set to (MEM_DEVICE_WRITE_ADDR ) ! */
-  /*---------------------------------------------------*/
-  
-  uint16_t addr16 = Addr;
-  uint16_t value;
-  uint16_t val = SerialEEPROM_ReadVariable(addr16, &value); 
-
-  uint8_t se = SerialEEPROM_24Cxx_Detect();
-#endif
-}
-
-
-#if 1
-void CatDriver_ft817_HandleProtocol()
-{
-  uint8_t bc = 0;
-  uint8_t resp[32];
-          
-  while (CatDriver_InterfaceBufferGetData(ft817.req,5))
-    {
-#ifdef DEBUG_FT817
-      int debug_idx;
-      for (debug_idx = 0; debug_idx < 5 && ft817.cmd_cntr < FT817_MAX_CMD; debug_idx++ )
-	{
-	  ft817.reqs[ft817.cmd_cntr*5+debug_idx] = ft817.req[debug_idx];
-	}
-      ft817.cmd_cntr++;
-#endif
-      
-      switch((Ft817_CatCmd_t)ft817.req[4])
-	{
-	case FT817_SET_FREQ:
-	  {
-	    ulong f = 0;
-	    ulong fdelta;
-	    
-	    if(ts.xlat == 0)
-	      {
-		fdelta = (ts.tx_audio_source == TX_AUDIO_DIGIQ)?AudioDriver_GetTranslateFreq()*4:0;
-		// If we are in DIGITAL IQ Output mode, use real tune frequency frequency instead
-		// translated RX frequency
-	      }
-	    else
-	      {
-		fdelta = 0;
-	      }
-	    
-	    int fidx;
-	    for (fidx = 0; fidx < 4; fidx++)
-	      {
-		f *= 100;
-		f +=  (ft817.req[fidx] >> 4) * 10 + (ft817.req[fidx] & 0x0f);
-	      }
-	    f *= TUNE_MULT*10;
-	    df.tune_new = f - fdelta;
-	    
-	    resp[0] = 0;
-	    bc = 1;
-	    if(ts.flags1 & FLAGS1_CAT_IN_SANDBOX)			// if running in sandbox store active band
-	      {
-		ts.cat_band_index = ts.band;
-	      }
-	  }
-	  break;
-	  
-	case FT817_GET_FREQ:
-	  {
-	    ulong fdelta;
-	    
-	    if(ts.xlat == 0)
-	      {
-		fdelta = (ts.tx_audio_source == TX_AUDIO_DIGIQ)?AudioDriver_GetTranslateFreq()*TUNE_MULT:0;
-		// If we are in DIGITAL IQ Output mode, send real tune frequency frequency instead
-		// translated RX frequency
-	      }
-	    else
-	      {
-		fdelta = 0;
-	      }
-	    
-	    ulong f = (df.tune_new + fdelta  + (TUNE_MULT*10/2))/ (TUNE_MULT*10);
-	    ulong fbcd = 0;
-	    int fidx;
-	    for (fidx = 0; fidx < 8; fidx++)
-	      {
-		fbcd >>= 4;
-		fbcd |= (f % 10) << 28;
-		f = f / 10;
-	      }
-	    
-	    resp[0] = (uint8_t)(fbcd >> 24);
-	    resp[1] = (uint8_t)(fbcd >> 16);
-	    resp[2] = (uint8_t)(fbcd >> 8);
-	    resp[3] = (uint8_t)fbcd;
-	  }
-	  switch(ts.dmod_mode)
-	    {
-	    case DEMOD_LSB:
-	      resp[4] = 0;
-	      break;
-	    case DEMOD_USB:
-	      resp[4] = 1;
-	      break;
-	    case DEMOD_CW:
-	      resp[4] = 2 + (ts.cw_lsb==true?1:0);
-	      break;
-	      // return 3 if CW in LSB aka CW-R
-	    case DEMOD_SAM:
-	    case DEMOD_AM:
-	      resp[4] = 4;
-	      break;
-	    case DEMOD_FM:
-	      resp[4] = 8;
-	      break;
-	    default:
-	      resp[4] = 1;
-	    }
-	  bc = 5;
-	  break;
-	case 7: /* set mode */
-	  {
-	    uint32_t new_mode = ts.dmod_mode;
-	    uint32_t new_lsb = ts.cw_lsb;
-	    switch (ft817.req[0])
-	      {
-	      case 0: // LSB
-		new_mode = DEMOD_LSB;
-		break;
-	      case 1: // USB
-		new_mode = DEMOD_USB;
-		break;
-	      case 2: // CW
-		new_lsb = false;
-		new_mode = DEMOD_CW;
-		break;
-	      case 3: // CW-R
-		new_lsb = true;
-		new_mode = DEMOD_CW;
-		break;
-	      case 4: // AM
-		new_mode = DEMOD_AM;
-		break;
-	      case 8: // FM
-	      case 0x88: // FM-N
-		new_mode = DEMOD_FM;
-		break;
-	      case 0x0a: // DIG - SSB, side band controlled by some menu configuration in ft817, we use USB here
-		new_mode = DEMOD_USB;
-		break;
-	      case 0x0c: // PKT - FM, 9k6
-		new_mode = DEMOD_FM;
-		break;
-	      }
-	    if  (new_mode != ts.dmod_mode || new_lsb != ts.cw_lsb )
-	      {
-		if(ts.flags1 & FLAGS1_CAT_IN_SANDBOX)			// if running in sandbox store active band
-		  {
-		    ts.cat_band_index = ts.band;
-		  }
-		ts.cw_lsb = new_lsb;
-		RadioManagement_SetDemodMode(new_mode);
-		UiDriver_UpdateDisplayAfterParamChange();
-	      }
-	  }
-	  break;
-	case FT817_PTT_ON:
-	  resp[0] = cat_driver.cat_ptt_active?0xF0:0x00;
-	  /* 0xF0 if PTT was already on */
-	  
-	  if(RadioManagement_IsTxDisabled() == false)
-	    {
-	      ts.ptt_req = true;
-	      cat_driver.cat_ptt_active = true;
-	    }
-	  
-	  bc = 1;
-	  break;
-	case FT817_PWR_ON:
-	  resp[0] = 0;
-	  bc = 1;
-	  break;
-	case FT817_TOGGLE_VFO:
-	  UiDriver_ToggleVfoAB();
-	  resp[0] = 0;
-	  bc = 1;
-	  break;
-	case FT817_SPLIT_ON:
-	  UiDriver_SetSplitMode(true);
-	  resp[0] = 0;
-	  bc = 1;
-	  break;
-	case FT817_SPLIT_OFF:
-	  UiDriver_SetSplitMode(false);
-	  resp[0] = 0;
-	  bc = 1;
-	  break;
-	case FT817_PTT_OFF:
-	  resp[0] = cat_driver.cat_ptt_active?0x00:0xF0; /* 0xF0 if PTT was already off */
-	  ts.ptt_req = false;
-	  cat_driver.cat_ptt_active = false;
-	  bc = 1;
-	  break;
-	case FT817_A7: /* A7 */
-	  resp[0]=0xA7;
-	  resp[1]=0x02;
-	  resp[2]=0x00;
-	  resp[3]=0x04;
-	  resp[4]=0x67;
-	  resp[5]=0xD8;
-	  resp[6]=0xBF;
-	  resp[7]=0xD8;
-	  resp[8]=0xBF;
-	  bc = 9;
-	  break;
-	case FT817_EEPROM_READ:
-	  resp[0]=0x00;
-	  bc = 1;
-	  break;
-	case FT817_EEPROM_WRITE:
-	  resp[0] = 0;
-	  bc = 1;
-	  break;
-	case FT817_READ_TX_STATE:
-	  if(RadioManagement_IsTxDisabled()||(ts.txrx_mode != TRX_MODE_TX))
-	    {
-	      resp[0] = 0;
-	    }
-	  else
-	    {
-	      resp[0] =((uint8_t)round(swrm.fwd_pwr)<<4)+(uint8_t)round(swrm.vswr_dampened);
-	    }
-	  bc = 1;
-	  break;
-	case FT817_READ_RX_STATE: /* E7 */
-	  resp[0] = (uint8_t)round(sm.s_count*0.5);	//S-Meter signal
-	  bc = 1;
-	  break;
-	case FT817_PTT_STATE:
-	  // FT-817 responds 0xFF if not TX and 0x00 if TX
-	  // This differs from KA7OEI description but has been verified
-	  // with the real thing.
-	  resp[0]=ts.txrx_mode == TRX_MODE_TX?0x00:0xFF;
-	  if(RadioManagement_IsTxDisabled())
-	    {
-	      resp[0] =0xFF;
-	    }
-	  bc = 1;
-	  break;
-	case 255: /* FF sent out by HRD */
-	  break;
-	  // default:
-	  // while (1);
-	}
-    }
-  CatDriver_InterfaceBufferPutData(resp,bc);
-  /* Return data back */
-}
-#endif
-
-
-
 
 class ft817c
 {
@@ -448,6 +218,7 @@ public:
       FT817_PTT_STATE,
       FT817_NOOP
     };
+  bool m_verbose = false;
 public:
   fill_ft817 ()
   {
@@ -458,7 +229,8 @@ public:
     fill_buf b;   
     b.buf[4]  = (uint8_t) cmd;
     b.add(5);
-    printf ("filldata 0x%x\n",cmd);
+    if (m_verbose)
+      printf ("filldata 0x%x\n",cmd);
     return true;
   };
   bool filldata_loop()
@@ -481,7 +253,7 @@ public:
   void reset_all()
   {
     cat_buffer_reset(); // input buffer
-    total_transmitt_Len = 0;  // output sent
+    transdata.tab.clear(); 
   };
 };
 
@@ -505,7 +277,7 @@ TEST(FillBuftest, fill_buf)
   glb.reset_all();
 }
 
-TEST(FillBuftest, allcommands) 
+TEST(CatDrivertest, allcommands) 
 {
   global_data glb;
 
@@ -514,16 +286,18 @@ TEST(FillBuftest, allcommands)
     {
       bool ok = t.filldata(*i);
       EXPECT_EQ (true, ok);
-      uint32_t prev = total_transmitt_Len;
-      // CatDriver_HandleProtocol();  have eat data!
-      CatDriver_ft817_HandleProtocol();
+
+      ts.sysclock  = cat_driver.lastbufferadd_time = CAT_DRIVER_TIMEOUT; // hack
+      CatDriver_HandleCommands();
+      size_t sz = transdata.tab.size(); 
+#if 0
       printf ("ts.tx_disable=0x%x  CatDriver_CatPttActive()=%d \n",ts.tx_disable, CatDriver_CatPttActive());
-      uint32_t Len = total_transmitt_Len - prev; 
-      if (*i == FT817_MODE_SET) // error !!
-	continue;
+      transdata.dump(); 
+#endif
+      transdata.tab.clear(); 
       if (*i == FT817_NOOP) // well
 	continue;
-      EXPECT_GT(Len,0);
+      EXPECT_GT(sz,0);
     }
 }
 
@@ -587,7 +361,8 @@ int main(int argc, char **argv)
 	  //	  f8.filldata(FT817_SET_FREQ);
 	  //	  f8.filldata(FT817_GET_FREQ);
 	  f8.filldata_loop();
-	  CatDriver_ft817_HandleProtocol();
+	  ts.sysclock  = cat_driver.lastbufferadd_time = CAT_DRIVER_TIMEOUT; 
+	  CatDriver_HandleCommands();
 	}
       //      CatDriver_HandleProtocol();
       uint8_t bytes = CatDriver_InterfaceBufferHasData();
